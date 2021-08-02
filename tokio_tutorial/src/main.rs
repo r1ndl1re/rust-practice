48f6ef0fb600108ff6e4859d3b7b3e4126b4bd7d
knishimori <rendlire@gmail.com>
2021-08-03 02:34:51 +0900

    wip

--- tokio_tutorial/src/main.rs
+++ tokio_tutorial/src/main.rs
@@ -1,30 +1,36 @@
-use core::panic;
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 use mini_redis::{Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     // リスナーをこのアドレスにバインドする
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
-        // タプルの2つ目の要素は、新しいコネクションのIPとポートの情報を含んでいる
         let (socket, _) = listener.accept().await.unwrap();
+
+        let db = db.clone();
+
+        println!("Accepted");
+
         tokio::spawn(async move {
-            // それぞれのインバウンドソケットに対して新しいタスクを spawn する
-            // ソケットは新しいタスクに move され、そこで処理される
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    // データを蓄えるため `HashMap` を使う
-    let mut db = HashMap::new();
 
     // `mini-redis` が提供するコネクションによって、ソケットから来るフレームをパースする
     let mut connection = Connection::new(socket);
@@ -33,11 +39,12 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                // `Vec<u8>` として保存する
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     Frame::Bulk(value.clone().into())
                 } else {
